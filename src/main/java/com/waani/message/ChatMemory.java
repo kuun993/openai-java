c98a2d685e5ffb59490ce57746d82dbe04c0b06e
waani <kuun993@163.com>
2024-09-05 09:35:03 +0800

    Create ChatMemory.java

---
+++ src/main/java/com/waani/message/ChatMemory.java
@@ -0,0 +1,31 @@
+package com.waani.memory;
+
+import com.waani.openai.message.Message;
+import com.waani.openai.message.SystemMessage;
+
+import java.util.List;
+
+/**
+ * @author waani
+ * @date 2024/9/4
+ */
+public abstract class ChatMessage {
+
+    public abstract void add(Message message);
+
+    public abstract List<Message> messages();
+
+    protected List<Message> addMessage(Message message) {
+        List<Message> messages = messages();
+        Message first = messages.get(0);
+        if (message instanceof SystemMessage) {
+            if (!(first instanceof SystemMessage)) {
+                messages.add(0, message);
+            }
+        } else {
+            messages.add(message);
+        }
+        return messages;
+    }
+
+}
